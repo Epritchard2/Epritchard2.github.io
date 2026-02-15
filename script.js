466a65c70eca0c6e1e995c32f306cd57a04a23fd
Evan Pritchard <epritchard2@ivytech.edu>
2026-02-15 12:46:02 -0500

    Update script.js
    swapped mobile fix to bottom of page instead of top

--- script.js
+++ script.js
@@ -3,10 +3,7 @@
  */
 
 console.log("Omnæs Dæmones website loaded successfully!");
-// Fix parallax on mobile
-if (/Mobi|Android/i.test(navigator.userAgent)) {
-  document.body.style.backgroundAttachment = 'scroll';
-}
+
 
 // ============================================================
 // UTILITY
@@ -288,3 +285,8 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 })();
+
+// Fix parallax on mobile
+if (/Mobi|Android/i.test(navigator.userAgent)) {
+  document.body.style.backgroundAttachment = 'scroll';
+}
